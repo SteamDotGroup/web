a08c07738b84464bb160e58225d743cff6ac7654
MarvinJWendt <github@marvinjwendt.com>
2018-12-02 17:12:36 +0100

    Refactor: New website design

--- index.php
+++ index.php
@@ -4,33 +4,53 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>SteamGroup</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/styles.min.css?h=0dc9bb3f767742f2edf15b77f487729d">
+    <title>SteamDotGroup | Steamgroup link shortener</title>
+    <meta name="description" content="Shortener for steamgroup links.">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/4.1.3/darkly/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bitter:400,700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="assets/css/styles.min.css?h=7fe014da11ec987340f785c7bdb62b09">
 </head>
 
-<body style="background-color: rgb(47,192,201);">
-    <!-- Start: 2 Rows 1+2 Columns -->
-    <div>
-        <div class="container" style="margin-top: 20%;">
-            <div class="row">
-                <div class="col-md-12">
-                    <h1 class="center">Steam Dot Group</h1>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-6">
-                    <p class="center">Without SteamDotGroup: <br>https://steamcommunity.com/groups/<strong>YourGroup</strong></p>
-                </div>
-                <div class="col-md-6">
-                    <p class="center">With SteamDotGroup: <br>https://steam.group/<strong>YourGroup</strong></p>
+<body style="color:#ffffff;">
+    <!-- Start: Loading Page Animation Style -->
+    <div id="LoadingPage" class="loading-icon" style="background-color:rgb(0,0,0);"><i class="fa fa-meh-o fa-spin fa-5x fa-fw"></i></div>
+    <!-- End: Loading Page Animation Style -->
+    <!-- Start: Bold BS4 Jumbotron with Particles js -->
+    <div class="jumbotron jumbotron-fluid jumbotron-main"><script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script> 
+    <div id="particles-js">
+	<canvas class="particles-js-canvas-el" width="1409" height="319" style="width: 100%; height: 100%;"></canvas>
+	</div>
+        <div class="container center-vertically-holder" style="margin-top:-20px;">
+            <div class="row center-vertically">
+                <div class="col-md-8 offset-sm-0 offset-md-2 text-center" style="margin-top:100px;margin-bottom:100px;">
+                    <h1 class="text-center" style="height:88px;">SteamDotGroup<br></h1>
+                    <hr style="width:340px;height:0px;margin-top:-51px;">
+                    <div>
+                        <!-- Start: Twitter --><a href="https://twitter.com/MarvinJWendt" target="_blank"></a>
+                        <!-- End: Twitter -->
+                        <p>Steamgroup shortlink generator.<br>Turn your long steamgroup link into a short one with <strong>SteamDotGroup</strong>!<br>Instead of writing: https://steamcommunity.com/groups/<strong>YourSteamGroup</strong><br>You can now simply
+                            use: https://steam.group/<strong>YourSteamGroup</strong><br><br><strong>Also we are on GitHub!</strong></p>
+                        <!-- Start: GitHub --><a href="https://github.com/SteamDotGroup" target="_blank"><i class="fa fa-github so-ico" data-bs-hover-animate="tada"></i></a>
+                        <!-- End: GitHub -->
+                        <!-- Start: Star --><a href="#"></a>
+                        <!-- End: Star -->
+                        <!-- Start: Instagram --><a href="https://www.instagram.com/marvinjwendt/" target="_blank"></a>
+                        <!-- End: Instagram -->
+                        <!-- Start: Steam --><a href="https://steamcommunity.com/id/MarvinJWendt" target="_blank"></a>
+                        <!-- End: Steam -->
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-    <!-- End: 2 Rows 1+2 Columns -->
+    <!-- End: Bold BS4 Jumbotron with Particles js -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/script.min.js?h=674d6b2afc6738c0310172ddfa6c5fcb"></script>
 </body>
 
 </html>
